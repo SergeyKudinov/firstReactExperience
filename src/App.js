822025e146136ed57096154a72480da5e63ed706
Администратор <ilnur@iMac-Denis.local>
2022-08-30 16:59:02 +0300

    add select and search

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import MyButton from './components/UI/button/MyButton';
 import './styles/App.css';
 import MyInput from './components/UI/input/MyInput';
 import PostForm from './components/PostForm';
+import MySelect from './components/UI/select/MySelect';
 
 function App() {
 
@@ -15,6 +16,20 @@ function App() {
     { id: 2, title: 'Javascript 2', body: 'Description' },
     { id: 3, title: 'Javascript 3', body: 'Description' },
   ])
+
+  const [selectedSort, setSelectedSort] = useState('')
+
+  const [searchQuery, setSearchQuery] = useState('')
+
+  function getSortedPosts() {
+    console.log("Fuck you");
+    if (selectedSort) {
+      return [...posts].sort((a, b) => a[selectedSort].localeCompare(b[selectedSort]))
+    }
+    return posts;
+  }
+
+  const sortedPosts = getSortedPosts();
 
   const createPost = (newPost) => {
     setPosts([...posts, newPost])
@@ -25,10 +40,34 @@ function App() {
     setPosts(posts.filter(p => p.id !== post.id))
   }
 
+  const sortPosts = (sort) => {
+    setSelectedSort(sort);
+  }
+
   return (
     <div className='App'>
       <PostForm create={createPost} />
-      <PostList remove={removePost} posts={posts} title="Посты про JS" />
+      <hr style={{ margin: "15px 0" }} />
+      <div>
+        <MyInput
+          value={searchQuery}
+          onChange={e => setSearchQuery(e.target.value)}
+          placeholder="Поиск..."
+        />
+        <MySelect
+          value={selectedSort}
+          onChange={sortPosts}
+          defaultValue="Сортировка"
+          options={[
+            { value: 'title', name: 'По названию' },
+            { value: 'body', name: 'По описанию' },
+          ]}
+        />
+      </div>
+      {posts.length
+        ? <PostList remove={removePost} posts={sortedPosts} title="Посты про JS" />
+        : <h1 style={{ textAlign: 'center' }}>Посты не найдены!</h1>
+      }
     </div>
   );
 }
