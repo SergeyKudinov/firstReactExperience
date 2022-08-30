604827c1dbba96dba4782209ffbd78b044e168bc
Администратор <ilnur@iMac-Denis.local>
2022-08-30 15:04:43 +0300

    post new posts

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
-import React, { useState } from 'react';
+import React, { useRef, useState } from 'react';
 import ClassCounter from './components/ClassCounter';
 import Counter from './components/Counter';
 import PostList from './components/PostList';
 import PostItem from './components/post_item';
 import MyButton from './components/UI/button/MyButton';
 import './styles/App.css';
+import MyInput from './components/UI/input/MyInput';
 
 function App() {
 
@@ -14,12 +15,39 @@ function App() {
     { id: 3, title: 'Javascript 3', body: 'Description' },
   ])
 
+  const [title, setTitle] = useState('');
+  const [body, setBody] = useState('');
+
+  const addNewPost = (e) => {
+    e.preventDefault();
+    const newPost = {
+      id: Date.now(),
+      title,
+      body
+    }
+    setPosts([...posts, newPost])
+    setTitle('')
+    setBody('')
+  };
+
   return (
     <div className='App'>
       <form>
-        <input type="text" placeholder='Название поста' />
-        <input type="text" placeholder='Описание поста' />
-        <MyButton disabled>Создать пост</MyButton>
+        {/* Управляемый компонент */}
+        <MyInput
+          value={title}
+          onChange={e => setTitle(e.target.value)}
+          type="text"
+          placeholder='Название поста' />
+
+        {/* Неуправляемый, неконтролируемый компонент */}
+        <MyInput
+          value={body}
+          onChange={e => setBody(e.target.value)}
+          type="text"
+          placeholder='Описание поста'
+        />
+        <MyButton onClick={addNewPost}>Создать пост</MyButton>
       </form>
       <PostList posts={posts} title="Посты про JS" />
     </div>
