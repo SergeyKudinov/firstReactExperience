ec403818f9e6c23227e90bee62fcf47db7166b78
Sergey <serezha.kudinov.1995@mail.ru>
2022-09-22 14:46:11 +0300

    add routes, move logic to Posts component

--- src/App.js
+++ src/App.js
@@ -1,62 +1,19 @@
-import React, { useMemo, useState } from 'react';
-import ClassCounter from './components/ClassCounter';
-import Counter from './components/Counter';
-import PostList from './components/PostList';
-import PostItem from './components/PostItem';
-import MyButton from './components/UI/button/MyButton';
+import React from 'react';
 import './styles/App.css';
-import MyInput from './components/UI/input/MyInput';
-import PostForm from './components/PostForm';
-import MySelect from './components/UI/select/MySelect';
-import PostFilter from './components/PostFilter';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import About from './pages/About';
+import Posts from './pages/Posts';
 
 function App() {
 
-  const [posts, setPosts] = useState([
-    { id: 1, title: 'Javascript', body: 'Description' },
-    { id: 2, title: 'Javascript 2', body: 'Description' },
-    { id: 3, title: 'Javascript 3', body: 'Description' },
-  ])
-
-  const [filter, setFilter] = useState({ sort: '', query: '' });
-
-  const sortedPosts = useMemo(() => {
-    if (filter.sort) {
-      return [...posts].sort((a, b) => a[filter.sort].localeCompare(b[filter.sort]))
-    }
-    return posts;
-  }, [filter.sort, posts]);
-
-  const sortedAndSearchedPosts = useMemo(() => {
-    return sortedPosts.filter(post => post.title.toLowerCase().includes(filter.query));
-  }, [filter.query, sortedPosts]);
-
-  const createPost = (newPost) => {
-    setPosts([...posts, newPost])
-  }
-
-  // Получаем post дочернего компонента
-  const removePost = (post) => {
-    setPosts(posts.filter(p => p.id !== post.id))
-  }
-
   return (
-    <div className='App'>
-      <PostForm create={createPost} />
-      <hr style={{ margin: "15px 0" }} />
-      <div>
-        <PostFilter
-          filter={filter}
-          setFilter={setFilter}
-        />
-        <PostList
-          remove={removePost}
-          posts={sortedAndSearchedPosts}
-          title="Посты про JS"
-        />
-      </div>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path='/about' element={<About />} />
+        <Route path='/posts' element={<Posts />} />
+      </Routes>
+    </BrowserRouter>
   );
-}
+};
 
 export default App;
