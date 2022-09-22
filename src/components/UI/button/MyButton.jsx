291b1c5f3d0ad74ad768bc113dbd122a8a4b190e
Sergey <serezha.kudinov.1995@mail.ru>
2022-09-22 15:01:40 +0300

    rename style files

--- src/components/UI/button/MyButton.jsx
+++ src/components/UI/button/MyButton.jsx
@@ -1,9 +1,8 @@
-import React from 'react';
-import classes from './MyButton.module.css'
+import './MyButton.css'
 
 const MyButton = ({ children, ...props }) => {
     return (
-        <button {...props} className={classes.myBtn}>
+        <button {...props} className='myBtn'>
             {children}
         </button>
     );
--- src/components/UI/input/MyInput.jsx
+++ src/components/UI/input/MyInput.jsx
@@ -1,9 +1,9 @@
 import React from "react";
-import classes from "./MyInput.module.css";
+import "./MyInput.css";
 
 const MyInput = React.forwardRef((props, ref) => {
     return (
-        <input ref={ref} className={classes.myInput} {...props} />
+        <input ref={ref} className='myInput' {...props} />
     );
 });
 
